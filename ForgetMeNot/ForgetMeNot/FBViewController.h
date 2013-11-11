b4ebf9d02861bfbe585084198b4a64248b5dd9d8
Jamie <jamiecorr@Macintosh.local>
2013-11-11 14:23:02 -0800

    new view controllers

---
+++ ForgetMeNot/ForgetMeNot/FBViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBViewController.h
+//  ForgetMeNot
+//
+//  Created by Jamie on 11/11/13.
+//  Copyright (c) 2013 Lucky7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBViewController : UIViewController
+
+@end
---
+++ ForgetMeNot/ForgetMeNot/FBViewController.m
@@ -0,0 +1,38 @@
+//
+//  FBViewController.m
+//  ForgetMeNot
+//
+//  Created by Jamie on 11/11/13.
+//  Copyright (c) 2013 Lucky7. All rights reserved.
+//
+
+#import "FBViewController.h"
+
+@interface FBViewController ()
+
+@end
+
+@implementation FBViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
