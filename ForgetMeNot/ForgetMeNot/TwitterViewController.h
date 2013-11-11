f852f3f619a508a7164c57b24c83448df2a12507
jccorr <jccorr@235x03.csc.calpoly.edu>
2013-11-11 14:18:08 -0800

    new view controller

---
+++ ForgetMeNot/ForgetMeNot/TwitterViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwitterViewController.h
+//  ForgetMeNot
+//
+//  Created by jccorr on 11/11/13.
+//  Copyright (c) 2013 Lucky7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitterViewController : UIViewController
+
+@end
---
+++ ForgetMeNot/ForgetMeNot/TwitterViewController.m
@@ -0,0 +1,38 @@
+//
+//  TwitterViewController.m
+//  ForgetMeNot
+//
+//  Created by jccorr on 11/11/13.
+//  Copyright (c) 2013 Lucky7. All rights reserved.
+//
+
+#import "TwitterViewController.h"
+
+@interface TwitterViewController ()
+
+@end
+
+@implementation TwitterViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
