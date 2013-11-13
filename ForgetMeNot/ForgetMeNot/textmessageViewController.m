ecf4e2d5c165cd02bc28f8f5e9ae3b4aa1be0160
Abby Schmiedt <abbyschmiedt@pcp072519pcs.wireless.calpoly.edu>
2013-11-12 16:27:04 -0800

    The array works in this example

--- ForgetMeNot/ForgetMeNot/textmessageViewController.m
+++ ForgetMeNot/ForgetMeNot/textmessageViewController.m
@@ -9,25 +9,38 @@
 #import "textmessageViewController.h"
 
 @interface textmessageViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *dateField;
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
 @property (weak, nonatomic) IBOutlet UITextView *messageField;
+@property NSMutableArray *messageArray;
 
 @end
 
 @implementation textmessageViewController
 - (IBAction)saveButton:(id)sender {
-    NSDictionary *dict = @{
-                           @"Name": self.nameField.text,
-                           @"PhoneNumber": self.numberField.text,
-                           @"Message":self.messageField.text,
-                           };
+    //this dictionary holds the recipients name, number, and message
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
     
+    [dict setObject:self.nameField.text forKey:@"Name"];
+    [dict setObject:self.dateField.date forKey:@"Date"];
+    [dict setObject:self.numberField.text forKey:@"Number"];
+    [dict setObject:self.messageField.text forKey:@"Message"];
+    
+    //This code writes the dictionary to a file in the phone
     NSString *rootPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    [self.messageArray addObject:dict];
     NSString *plistPath = [rootPath stringByAppendingPathComponent:@"Data.plist"];
-    [dict writeToFile:plistPath atomically:YES];
-    NSLog (@"%@", dict);
+    [self.messageArray writeToFile:plistPath atomically:YES];
+    
+    //This array holds the different messages
+    
+    
+    NSLog(@"%@", self.messageArray);
+
+    
 }
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -40,8 +53,16 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.messageArray = [[NSMutableArray alloc] init];
+    NSString *rootPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *plistPath = [rootPath stringByAppendingPathComponent:@"Data.plist"];
+    self.messageArray = [[NSMutableArray alloc] initWithContentsOfFile:plistPath];
+    if(self.messageArray==nil){
+        self.messageArray = [[NSMutableArray alloc] init];
+    }
+    [super viewDidLoad];
+    
 }
 
 - (void)didReceiveMemoryWarning
